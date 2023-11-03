2f5e3e979853552d3bdbb1646a2da1f66f0093c4
FabiMi <91958341+FabiMi@users.noreply.github.com>
2023-11-03 13:44:10 +0100

    changed the imported component to FoodMainView

--- bellas_tool/src/components/main-view/main-view.jsx
+++ bellas_tool/src/components/main-view/main-view.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useState } from "react";
-import { FoodCard } from '../food-card/food-card';  
+import { FoodMainView } from '../food-main-view/food-main-view';     
 
 export const MainView = () => {
   
 return (
     <div className="main-view">
-      <FoodCard foodItems />
+      <FoodMainView />
     </div>
   );
 
